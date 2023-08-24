a0d66d3a6c3d93dee9cf974cde83d4f3a9ace1c8
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-08-25 00:56:57 +0100

    ADD post custom methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,11 @@ class Post < ApplicationRecord
   has_many :like
   has_many :comment
 
-  
+  def recent_5_comments
+    comment.order(created_at: :desc).limit(5)
+  end
+
+  def increment_post_count
+    author.increment!(:post_counter)
+  end
 end
