d6bc3eb1fd33996631050e308482335eedfa7c57
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-08-25 00:58:34 +0100

    ADD custom comment methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post, counter_cache: :comments_counter
 
- 
+  def increment_comment_count
+    post.increment!(:comment_counter)
+  end
 end
