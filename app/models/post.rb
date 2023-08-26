1fa7ca1162ff6f27dbe84295f46cd8af350bf69d
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-08-26 01:59:35 +0100

    ADD post validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,9 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', counter_cache: :post_counter
   has_many :like
   has_many :comment
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comment_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :like_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_5_comments
     comment.order(created_at: :desc).limit(5)
