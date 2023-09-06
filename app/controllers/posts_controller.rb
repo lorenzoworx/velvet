d31fe8b974032c2582eadbdd8bb69b2d10f7b95b
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-09-06 23:20:50 +0100

    ADD new methods to posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,29 @@ class PostsController < ApplicationController
   end
 
   def show
+    # @user = User.find_by(id: params[:user_id])
     @post = Post.where(author_id: params[:user_id], id: params[:id]).first
+    # @current_user = current_user
+    @like = Like.new
+    @comment = Comment.new
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.post.new(post_param)
+    if @post.save
+      redirect_to user_post_path(current_user, @post)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_param
+    params.require(:post).permit(:title, :text)
   end
 end
