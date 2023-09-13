8e82313967a18a0e99e2cd5535b3def864db6dcc
ProgramKingAbel <abelmorara254@gmail.com>
2023-09-13 19:09:23 +0300

    Fix rubocop linters

--- spec/system/post_show_spec.rb
+++ spec/system/post_show_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe "Post", type: :system do
-  describe "Show Posts Page" do
+RSpec.describe 'Post', type: :system do
+  describe 'Show Posts Page' do
     before do
       @user = User.create(name: 'Jane', bio: 'Full-stack developer', photo: 'https://placehold.co/100x100')
       @first_post = Post.create(author_id: @user.id, title: 'First post', text: 'First post')
@@ -16,28 +16,28 @@ RSpec.describe "Post", type: :system do
     it "Can see a post's title" do
       expect(page).to have_content(@first_post.title)
     end
-    
-    it "Can see who wrote the post" do
+
+    it 'Can see who wrote the post' do
       expect(page).to have_content("by #{@user.name}")
     end
 
-    it "Can see how many comments it has" do
+    it 'Can see how many comments it has' do
       expect(page).to have_content('Comments: 1')
     end
 
-    it "Can see how many likes it has" do
+    it 'Can see how many likes it has' do
       expect(page).to have_content('Likes: 1')
     end
 
-    it "Can see the post body" do
+    it 'Can see the post body' do
       expect(page).to have_content(@first_post.text)
     end
 
-    it "Can see the username of each commenter" do
-      expect(page).to have_content("#{@user.name}")
+    it 'Can see the username of each commenter' do
+      expect(page).to have_content(@user.name.to_s)
     end
 
-    it "Can see the comment left by each commenter" do
+    it 'Can see the comment left by each commenter' do
       expect(page).to have_content("#{@user.name}: #{@comment.text}")
     end
   end
--- spec/system/user_show_spec.rb
+++ spec/system/user_show_spec.rb
@@ -7,45 +7,45 @@ RSpec.describe 'Users', type: :system do
       @first_post = Post.create(author: @first_user, title: 'Test post 1', text: 'This is test post 1')
       Post.create(author: @first_user, title: 'Test post 2', text: 'This is test post 2')
       Post.create(author: @first_user, title: 'Test post 3', text: 'This is test post 3')
-      visit user_path(@first_user)      
+      visit user_path(@first_user)
     end
 
-    it "Can see the username" do
+    it 'Can see the username' do
       expect(page).to have_content(@first_user.name)
     end
 
-    it "Can see the user profile picture" do
+    it 'Can see the user profile picture' do
       expect(page).to have_css("img[src='https://placehold.co/100x100']")
     end
 
-    it "Can see the number of posts written by the user" do
-      expect(page).to have_content("Number of posts: 3")
+    it 'Can see the number of posts written by the user' do
+      expect(page).to have_content('Number of posts: 3')
     end
 
-    it "Can see the user bio" do
+    it 'Can see the user bio' do
       expect(page).to have_content(@first_user.bio)
     end
 
-    it "Can see the user first 3 posts" do
-      expect(page).to have_content("Test post 1")
-      expect(page).to have_content("Test post 2")
-      expect(page).to have_content("Test post 3")
-      expect(page).to have_content("This is test post 1")
-      expect(page).to have_content("This is test post 2")
-      expect(page).to have_content("This is test post 3")
+    it 'Can see the user first 3 posts' do
+      expect(page).to have_content('Test post 1')
+      expect(page).to have_content('Test post 2')
+      expect(page).to have_content('Test post 3')
+      expect(page).to have_content('This is test post 1')
+      expect(page).to have_content('This is test post 2')
+      expect(page).to have_content('This is test post 3')
     end
 
-    it "Can see a show more posts button that display all a users posts" do
-      expect(page).to have_link("See all posts")
+    it 'Can see a show more posts button that display all a users posts' do
+      expect(page).to have_link('See all posts')
     end
 
-    it "Redirects to the post show page when a user post clicked" do
-      click_link "Test post 1"
+    it 'Redirects to the post show page when a user post clicked' do
+      click_link 'Test post 1'
       expect(page).to have_current_path(user_post_path(@first_user, @first_post))
     end
 
-    it "Redirects to the user post index page" do
-      click_link "See all posts"
+    it 'Redirects to the user post index page' do
+      click_link 'See all posts'
       expect(page).to have_current_path(user_posts_path(@first_user))
     end
   end
