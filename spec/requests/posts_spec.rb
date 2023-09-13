376d74b6f2096e2ad7e52bc07a05dfb0598072b0
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-09-13 16:28:56 +0100

    FIX testing bugs

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Posts', type: :request do
   describe 'GET index' do
     before :all do
-      @test_user = User.create(name: 'John', photo: 'https://unsplash.com/photos', bio: 'Fullstack developer.')
+      @test_user = User.create(name: 'John', photo: 'https://placehold.co/100x100', bio: 'Fullstack developer.')
     end
 
     it 'should receive a success response.' do
@@ -16,26 +16,17 @@ RSpec.describe 'Posts', type: :request do
       expect(response).to render_template(:index)
     end
 
-    it 'renders correct placeholder text' do
-      get user_posts_path(@test_user)
-      expect(response.body).to include('Here is a list of posts made by the same user')
-    end
   end
 
   describe 'GET show' do
     before :all do
-      @test_user = User.create(name: 'John', photo: 'https://unsplash.com/photos', bio: 'Fullstack developer.')
+      @test_user = User.create(name: 'John', photo: 'https://placehold.co/100x100', bio: 'Fullstack developer.')
       @test_post = Post.create(author: @test_user, title: 'Hello World', text: 'This is my first post')
     end
 
     it 'should receive a success response' do
       get user_post_path(@test_user, @test_post)
       expect(response).to have_http_status(200)
-    end
-
-    it 'renders correct placeholder text' do
-      get user_post_path(@test_user, @test_post)
-      expect(response.body).to include('User post details')
     end
 
     it 'renders correct template' do
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -6,12 +6,7 @@ RSpec.describe 'Users', type: :request do
       get users_path
       expect(response).to have_http_status(200)
     end
-
-    it 'renders correct placeholder text' do
-      get users_path
-      expect(response.body).to include('Here is a list of all the Users')
-    end
-
+    
     it 'renders correct template' do
       get users_path
       expect(response).to render_template(:index)
@@ -20,21 +15,16 @@ RSpec.describe 'Users', type: :request do
 
   describe 'GET /users/:id' do
     before :all do
-      @test_user = User.create(name: 'John', photo: 'https://unsplash.com/photos', bio: 'Fullstack developer.')
+      @test_user = User.create(name: 'John', photo: 'https://placehold.co/100x100', bio: 'Fullstack developer.')
     end
 
     it 'should receive a success response' do
       get user_url(@test_user)
       expect(response).to have_http_status(200)
     end
-    it 'renders correct placeholder text' do
-      get user_url(@test_user)
 
-      expect(response.body).to include('User details')
-    end
     it 'renders correct template' do
       get user_url(@test_user)
-
       expect(response).to render_template(:show)
     end
   end
