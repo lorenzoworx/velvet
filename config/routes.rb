9ee6b801dca3e336415405a58c3b5e762f6aa6e9
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-09-15 11:08:04 +0100

    UPDATE routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'users#index'
-  resources :users, only: [:index, :show] do
+  resources :users, only: [:show, :edit, :update] do
     resources :posts, only: [:index, :show]
   end
 
